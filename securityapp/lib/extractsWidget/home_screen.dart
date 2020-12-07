3146e9a20c043d6b05a08dc89d5e118d0659631f
Asncodes-80 <40909549+Asncodes-80@users.noreply.github.com>
2020-12-07 09:06:21 +0330

    Improve botton navigation bar

--- securityapp/lib/extractsWidget/home_screen.dart
+++ securityapp/lib/extractsWidget/home_screen.dart
@@ -4,8 +4,10 @@ import 'package:mjpeg/mjpeg.dart';
 import 'package:securityapp/constFile/texts.dart';
 import '../adding_data.dart';
 import '../constFile/ConstFile.dart';
+import 'package:securityapp/extractsWidget/optStyle.dart';
 import 'extract_main_design.dart';
 
+// Service Section in HomeScreen (main)
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -159,6 +161,60 @@ class HomeScreen extends StatelessWidget {
                 ),
               ],
             ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+// Setting Section in HomeScreen (main)
+class Setting extends StatelessWidget {
+  const Setting({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(bottom: 12),
+      child: Column(
+        textDirection: TextDirection.rtl,
+        children: [
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+            child: Row(
+              textDirection: TextDirection.rtl,
+              children: [
+                Text(
+                  ':تنظمیات',
+                  style: TextStyle(
+                      fontFamily: 'BYekan',
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold),
+                )
+              ],
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
+              Navigator.pushNamed(context, '/StylePage');
+            },
+            child: OptionsViewer(
+              text: 'حالت شب و روز',
+              desc:
+                  "شما میتوانید با استفاده از این گزینه از اپلیکیشن در هر محیطی متناسب با چشمان خود استفاده کنید",
+              avatarBgColor: Colors.purple,
+              avatarIcon: LineAwesomeIcons.lightbulb,
+              iconColor: Colors.white,
+            ),
+          ),
+          OptionsViewer(
+            text: 'آدرس های آی پی',
+            desc: "برای دسترسی به تمامی آدرس های آی پی کلیک کنید",
+            avatarBgColor: Colors.yellow,
+            avatarIcon: Icons.privacy_tip_outlined,
+            iconColor: Colors.black,
           ),
         ],
       ),
--- securityapp/lib/security_app.dart
+++ securityapp/lib/security_app.dart
@@ -2,7 +2,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:line_awesome_flutter/line_awesome_flutter.dart';
 import 'package:hexcolor/hexcolor.dart';
-import 'package:securityapp/extractsWidget/optStyle.dart';
 import 'extractsWidget/home_screen.dart';
 import 'titleStyle/titles.dart';
 import 'constFile/texts.dart';
@@ -14,7 +13,15 @@ class InputSecurityApp extends StatefulWidget {
 }
 
 class _InputSecurityAppState extends State<InputSecurityApp> {
+  // Index for count of 0..1 between difference screen
   int tabBarIndex = 0;
+
+  // PageController for control two main screen in input page
+  // and Navigate between!
+  var _pageController = PageController();
+
+  // Following is a list for two page as Widget
+  var screens = [HomeScreen(), Setting()];
 
   @override
   Widget build(BuildContext context) {
@@ -32,18 +39,11 @@ class _InputSecurityAppState extends State<InputSecurityApp> {
         child: PageView(
           onPageChanged: (value) {
             setState(() {
-              if (tabBarIndex == 0) {
-                tabBarIndex = 1;
-              } else {
-                tabBarIndex = 0;
-              }
+              tabBarIndex = value;
             });
           },
-          children: [
-            tabBarIndex ? 0 : print('asdasd'): print('sada')
-            HomeScreen(),
-            Setting(),
-          ],
+          controller: _pageController,
+          children: screens,
         ),
       ),
       bottomNavigationBar: ClipRRect(
@@ -64,6 +64,8 @@ class _InputSecurityAppState extends State<InputSecurityApp> {
           onTap: (currentIndex) {
             setState(() {
               tabBarIndex = currentIndex;
+              _pageController.animateToPage(tabBarIndex,
+                  duration: Duration(milliseconds: 500), curve: Curves.ease);
             });
           },
           items: [
@@ -83,59 +85,6 @@ class _InputSecurityAppState extends State<InputSecurityApp> {
             ),
           ],
         ),
-      ),
-    );
-  }
-}
-
-class Setting extends StatelessWidget {
-  const Setting({
-    Key key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(bottom: 12),
-      child: Column(
-        textDirection: TextDirection.rtl,
-        children: [
-          Container(
-            margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-            child: Row(
-              textDirection: TextDirection.rtl,
-              children: [
-                Text(
-                  ':تنظمیات',
-                  style: TextStyle(
-                      fontFamily: 'BYekan',
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold),
-                )
-              ],
-            ),
-          ),
-          GestureDetector(
-            onTap: () {
-              Navigator.pushNamed(context, '/StylePage');
-            },
-            child: OptionsViewer(
-              text: 'حالت شب و روز',
-              desc:
-              "شما میتوانید با استفاده از این گزینه از اپلیکیشن در هر محیطی متناسب با چشمان خود استفاده کنید",
-              avatarBgColor: Colors.purple,
-              avatarIcon: LineAwesomeIcons.lightbulb,
-              iconColor: Colors.white,
-            ),
-          ),
-          OptionsViewer(
-            text: 'آدرس های آی پی',
-            desc: "برای دسترسی به تمامی آدرس های آی پی کلیک کنید",
-            avatarBgColor: Colors.yellow,
-            avatarIcon: Icons.privacy_tip_outlined,
-            iconColor: Colors.black,
-          ),
-        ],
       ),
     );
   }
