3b0a288e9e0ec2efa6bf8b229712e605a23d3667
Asncodes-80 <40909549+Asncodes-80@users.noreply.github.com>
2020-12-19 14:35:56 +0330

    rtsp ready to use in vlc player streamer

--- securityapp/lib/camera_grid.dart
+++ securityapp/lib/camera_grid.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_vlc_player/flutter_vlc_player.dart';
-// import 'package:flutter_vlc_player/flutter_vlc_player.dart';
 import 'package:securityapp/constFile/ConstFile.dart';
 
 class CameraGridView extends StatefulWidget {
@@ -9,28 +8,46 @@ class CameraGridView extends StatefulWidget {
 }
 
 class _CameraGridViewState extends State<CameraGridView> {
-
-  VlcPlayerController _vlcPlayerController = VlcPlayerController();
+  VlcPlayerController controller;
+  String urlToStream = "https://www.radiantmediaplayer.com/media/big-buck-bunny-360p.mp4";
+  @override
+  void initState() {
+    controller = VlcPlayerController(
+      onInit: (){
+        controller.play();
+      }
+    );
+    super.initState();
+  }
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "دوربین ها",
-          style: TextStyle(fontFamily: mainFontFamily),
+        appBar: AppBar(
+          title: Text(
+            "دوربین ها",
+            style: TextStyle(fontFamily: mainFontFamily),
+          ),
         ),
-      ),
-      body: Column(
-        children: [
-          VlcPlayer(
-            controller: _vlcPlayerController,
-            placeholder: Center(child: CircularProgressIndicator()),
-            aspectRatio: 25.5,
-            url: "http://213.226.254.135:91/mjpg/video.mjpg",
-          )
-        ],
-      )
-    );
+        body: Column(
+          children: [
+            SizedBox(
+              width: 640,
+              height: 360,
+              child: VlcPlayer(
+                aspectRatio: 16 / 9,
+                // put here your rtsp ip camera
+                url: urlToStream,
+                controller: controller,
+                placeholder: Center(child: CircularProgressIndicator()),
+              ),
+            ),
+          ],
+        ));
   }
 }
