fabe7bf3b3a9a493700ae01dcbfb287e4f1c4622
Asncodes-80 <40909549+Asncodes-80@users.noreply.github.com>
2021-04-06 18:57:37 +0430

    Remove some printer log

--- users_pwa/lib/controller/reserveController.dart
+++ users_pwa/lib/controller/reserveController.dart
@@ -52,7 +52,7 @@ void reserveMe({st, et, pt, context, bool themeChange}) async {
     try {
       String reserveResult = await api.reserveByUser(
           token: userToken, startTime: st, endTime: et, plateNo: pt);
-      print("$reserveResult");
+      // print("$reserveResult");
       if (reserveResult == "200") {
         alert(
             context: context,
