2c7118f88b97a24601d821ac47b9584b2cc46bcb
Asncodes-80 <40909549+Asncodes-80@users.noreply.github.com>
2021-04-18 16:49:21 +0430

    Change Add Plate bottom navigation bar to my plate for listing users plates

--- payausers/lib/Screens/Tabs/addUserPlate.dart
+++ payausers/lib/Screens/Tabs/addUserPlate.dart
@@ -1,20 +1,22 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:payausers/Classes/AlphabetClassList.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:lottie/lottie.dart';
+import 'package:payausers/Classes/ApiAccess.dart';
 import 'package:payausers/Classes/ThemeColor.dart';
 import 'package:payausers/ConstFiles/constText.dart';
 import 'package:payausers/ConstFiles/initialConst.dart';
-import 'package:payausers/ExtractedWidgets/dropdownMenu.dart';
-import 'package:payausers/controller/addPlateController.dart';
+import 'package:adaptive_action_sheet/adaptive_action_sheet.dart';
+import 'package:payausers/ConstFiles/initialConst.dart';
+import 'package:payausers/ExtractedWidgets/plateViwer.dart';
+import 'package:payausers/controller/alert.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 
-String plate0 = "";
-String plate1 = "";
-String plate2 = "";
-String plate3 = "";
-dynamic themeChange;
-int _value = 0;
-AlphabetList alp = AlphabetList();
+List userPlates = [];
+ApiAccess api = ApiAccess();
+FlutterSecureStorage lds = FlutterSecureStorage();
 
 class AddUserPlate extends StatefulWidget {
   @override
@@ -24,261 +26,179 @@ class AddUserPlate extends StatefulWidget {
 class _AddUserPlateState extends State<AddUserPlate> {
   @override
   void initState() {
-    plate0 = "";
-    plate1 = "";
-    plate2 = "";
-    plate3 = "";
-    _value = 0;
-    SystemChannels.textInput.invokeMethod('TextInput.hide');
     super.initState();
+
+    gettingMyPlates().then((plate) {
+      setState(() {
+        userPlates = plate;
+      });
+    });
   }
 
-  @override
-  void dispose() {
-    super.dispose();
+  Future<List> gettingMyPlates() async {
+    ApiAccess api = ApiAccess();
+    FlutterSecureStorage lds = FlutterSecureStorage();
+    final userToken = await lds.read(key: "token");
+    final plates = await api.getUserPlate(token: userToken);
+    return plates;
   }
 
   @override
   Widget build(BuildContext context) {
-    themeChange = Provider.of<DarkThemeProvider>(context);
-    return GestureDetector(
-      onTap: () => FocusScope.of(context).unfocus(),
-      child: SafeArea(
-          child: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Container(
-                    margin: EdgeInsets.symmetric(horizontal: 10),
-                    child: Text(
-                      addUserPlate,
-                      style: TextStyle(
+    final themeChange = Provider.of<DarkThemeProvider>(context);
+
+    // print(userPlates);
+    void delUserPlate(id) async {
+      try {
+        final userToken = await lds.read(key: "token");
+        final delStatus = await api.delUserPlate(token: userToken, id: id);
+        if (delStatus == "200") {
+          alert(
+              context: context,
+              aType: AlertType.success,
+              title: delProcSucTitle,
+              desc: delProcDesc,
+              themeChange: themeChange,
+              dstRoute: "dashboard");
+        }
+      } catch (e) {
+        alert(
+            aType: AlertType.warning,
+            title: delProcFailTitle,
+            desc: delProcFailDesc);
+      }
+    }
+
+    Widget plates = ListView.builder(
+      shrinkWrap: true,
+      itemCount: userPlates.length,
+      itemBuilder: (BuildContext context, index) {
+        return (Slidable(
+          actionPane: SlidableDrawerActionPane(),
+          actionExtentRatio: 0.25,
+          fastThreshold: 1.25,
+          movementDuration: Duration(milliseconds: 200),
+          child: Container(
+              color: themeChange.darkTheme ? darkBar : lightBar,
+              child: PlateViewer(
+                  plate0: userPlates[index]['plate0'] != null
+                      ? userPlates[index]['plate0']
+                      : "",
+                  plate1: userPlates[index]['plate1'] != null
+                      ? userPlates[index]['plate1']
+                      : "",
+                  plate2: userPlates[index]['plate2'] != null
+                      ? userPlates[index]['plate2']
+                      : "",
+                  plate3: userPlates[index]['plate3'] != null
+                      ? userPlates[index]['plate3']
+                      : "",
+                  themeChange: themeChange.darkTheme)),
+          actions: <Widget>[
+            IconSlideAction(
+              caption: 'پاک کردن',
+              color: Colors.red,
+              icon: Icons.delete,
+              onTap: () {
+                showAdaptiveActionSheet(
+                  context: context,
+                  title: Text(
+                    'پاک شود؟',
+                    style:
+                        TextStyle(fontFamily: mainFaFontFamily, fontSize: 20),
+                  ),
+                  actions: <BottomSheetAction>[
+                    BottomSheetAction(
+                        title: 'پاک کردن',
+                        textStyle: TextStyle(
+                          fontFamily: mainFaFontFamily,
+                          fontSize: 20,
+                          color: Colors.red,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        onPressed: () {
+                          delUserPlate(userPlates[index]['id']);
+                        }),
+                  ],
+                  cancelAction: CancelAction(
+                    title: 'لغو',
+                    textStyle: TextStyle(
                         fontFamily: mainFaFontFamily,
-                        fontSize: subTitleSize,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+                        color: Colors.blue,
+                        fontSize: 20),
+                  ), // onPressed parameter is optional by default will dismiss the ActionSheet
+                );
+              },
             ),
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Container(
-                    margin: EdgeInsets.symmetric(horizontal: 10),
-                    child: Text(
-                      yourPlateNumber,
-                      style: TextStyle(
-                          fontFamily: mainFaFontFamily, fontSize: subTitleSize),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              width: double.infinity,
-              height: 70,
-              // margin: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-              margin: EdgeInsets.only(top: 10, right: 20, left: 20, bottom: 5),
-              decoration: BoxDecoration(
-                  border: Border.all(
-                      color:
-                          themeChange.darkTheme ? Colors.white : Colors.black,
-                      width: 2.8),
-                  borderRadius: BorderRadius.circular(8)),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Container(
-                    width: 50,
-                    height: 70,
-                    decoration: BoxDecoration(
-                      color: Colors.blue[900],
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        SizedBox(height: 2),
-                        Image.asset(
-                          "assets/images/iranFlag.png",
-                          width: 35,
-                        ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              'I.R.',
-                              style: TextStyle(color: Colors.white),
-                            ),
-                            Text(
-                              'I R A N',
-                              style: TextStyle(color: Colors.white),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                  Container(
-                    width: 50,
-                    height: 70,
-                    margin: EdgeInsets.only(top: 0),
-                    child: TextFormField(
-                      style: TextStyle(
-                          fontSize: 26,
-                          fontFamily: mainFaFontFamily,
-                          fontWeight: FontWeight.bold),
-                      textAlign: TextAlign.center,
-                      initialValue: plate0,
-                      decoration: InputDecoration(
-                          counterText: "", border: InputBorder.none),
-                      maxLength: 2,
-                      keyboardType: TextInputType.number,
-                      onChanged: (value) {
-                        setState(() {
-                          plate0 = value;
-                        });
-                      },
-                    ),
-                  ),
-                  Container(
-                    width: 60,
-                    height: 70,
-                    margin: EdgeInsets.only(top: 7),
-                    child: DropdownButtonHideUnderline(
-                      child: DropdownButton(
-                        value: _value,
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontFamily: mainFaFontFamily,
-                            color: themeChange.darkTheme
-                                ? Colors.white
-                                : Colors.black,
-                            fontSize: 22),
-                        items: dropdownMenu,
-                        onChanged: (value) {
-                          setState(() {
-                            _value = value;
-                            // print(_value);
-                          });
-                        },
-                      ),
-                    ),
-                  ),
-                  Container(
-                    width: 50,
-                    height: 70,
-                    margin: EdgeInsets.only(top: 0, right: 0),
-                    child: TextFormField(
-                      initialValue: plate2,
-                      style: TextStyle(
-                          fontSize: 26,
-                          fontFamily: mainFaFontFamily,
-                          fontWeight: FontWeight.bold),
-                      textAlign: TextAlign.center,
-                      decoration: InputDecoration(
-                          counterText: "", border: InputBorder.none),
-                      maxLength: 3,
-                      keyboardType: TextInputType.number,
-                      onChanged: (value) {
-                        setState(() {
-                          plate2 = value;
-                        });
-                      },
-                    ),
-                  ),
-                  VerticalDivider(
-                      width: 1,
-                      color:
-                          themeChange.darkTheme ? Colors.white : Colors.black,
-                      thickness: 3),
-                  Container(
-                    width: 50,
-                    height: 70,
-                    margin: EdgeInsets.only(top: 0, right: 10),
-                    child: TextFormField(
-                      initialValue: plate3,
-                      style: TextStyle(
-                          fontSize: 26,
-                          fontFamily: mainFaFontFamily,
-                          fontWeight: FontWeight.bold),
-                      textAlign: TextAlign.center,
-                      decoration: InputDecoration(
-                          counterText: "", border: InputBorder.none),
-                      maxLength: 2,
-                      keyboardType: TextInputType.number,
-                      onChanged: (value) {
-                        setState(() {
-                          plate3 = value;
-                        });
-                      },
-                    ),
-                  ),
-                ],
-                crossAxisAlignment: CrossAxisAlignment.center,
-                textBaseline: TextBaseline.alphabetic,
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.symmetric(horizontal: 30),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Text(
-                    correctEntry,
-                    style: TextStyle(
-                      fontFamily: mainFaFontFamily,
-                    ),
-                    textAlign: TextAlign.right,
-                  ),
-                ],
-              ),
-            ),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: Container(
-                margin: EdgeInsets.symmetric(horizontal: 20, vertical: 100),
-                child: Material(
-                  elevation: 10.0,
-                  borderRadius: BorderRadius.circular(8.0),
-                  color: primarySubmitBtnColor,
-                  child: MaterialButton(
-                      padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-                      onPressed: () {
-                        sendNewUserPlate(plate0, alp.getAlphabet()[_value].item,
-                            plate2, plate3, context, themeChange, "dashboard");
-                      },
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            submitPlate,
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                                color: loginBtnTxtColor,
-                                fontFamily: mainFaFontFamily,
-                                fontSize: btnSized,
-                                fontWeight: FontWeight.bold),
-                          ),
-                          Icon(
-                            Icons.chevron_right,
-                            color: Colors.white,
-                          )
-                        ],
-                      )),
-                ),
-              ),
-            )
           ],
+        ));
+      },
+    );
+
+    Widget searchingProcess = Column(
+      children: [
+        Lottie.asset("assets/lottie/searching.json"),
+        Text(searchingProcessText,
+            style: TextStyle(fontFamily: mainFaFontFamily, fontSize: 18)),
+      ],
+    );
+
+    final plateContext = userPlates.isEmpty ? searchingProcess : plates;
+
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              AppBarAsNavigate(),
+              plateContext,
+            ],
+          ),
         ),
-      )),
+      ),
+    );
+  }
+}
+
+class AppBarAsNavigate extends StatelessWidget {
+  const AppBarAsNavigate({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Directionality(
+      textDirection: TextDirection.rtl,
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+        child:
+            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+          Text(
+            myPlateText,
+            style: TextStyle(
+                fontFamily: mainFaFontFamily,
+                fontSize: subTitleSize,
+                fontWeight: FontWeight.bold),
+          ),
+          ClipOval(
+            child: Material(
+              color: mainCTA, // button color
+              child: InkWell(
+                splashColor: mainSectionCTA, // inkwell color
+                child: SizedBox(
+                    width: 46,
+                    height: 46,
+                    child: Icon(
+                      Icons.add,
+                      color: Colors.white,
+                    )),
+                onTap: () =>
+                    Navigator.pushNamed(context, "/addUserplateAlternative"),
+              ),
+            ),
+          ),
+        ]),
+      ),
     );
   }
 }
