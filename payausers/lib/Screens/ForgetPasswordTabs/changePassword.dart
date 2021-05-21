359bfefaae1354c990a0d8479ef35a1aaf56ea15
Asncodes-80 <asn80.asn@hotmail.com>
2021-05-21 11:53:07 +0430

    Change Pop of forget password

--- payausers/lib/Screens/ForgetPasswordTabs/changePassword.dart
+++ payausers/lib/Screens/ForgetPasswordTabs/changePassword.dart
@@ -46,70 +46,75 @@ class _ChangePasswordState extends State<ChangePassword> {
   Widget build(BuildContext context) {
     otpCode = ModalRoute.of(context).settings.arguments;
     print("Your otp code : $otpCode");
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: mainCTA,
-        centerTitle: true,
-        title: Text(
-          "ویرایش گذرواژه",
-          style:
-              TextStyle(fontFamily: mainFaFontFamily, fontSize: subTitleSize),
+    return WillPopScope(
+      onWillPop: () {
+        Navigator.popUntil(context, ModalRoute.withName("/login"));
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: mainCTA,
+          centerTitle: true,
+          title: Text(
+            "ویرایش گذرواژه",
+            style:
+                TextStyle(fontFamily: mainFaFontFamily, fontSize: subTitleSize),
+          ),
         ),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(height: 2.0.h),
-            TextFields(
-                lblText: passwordPlaceHolder,
-                maxLen: 20,
-                keyboardType: TextInputType.visiblePassword,
-                readOnly: false,
-                textInputType: protectedPassword,
-                textFieldIcon:
-                    password == "" ? Icons.vpn_key_outlined : showMePass,
-                iconPressed: () {
-                  setState(() {
-                    protectedPassword
-                        ? protectedPassword = false
-                        : protectedPassword = true;
-                    // Changing eye icon pressing
-                    showMePass == Icons.remove_red_eye
-                        ? showMePass = Icons.remove_red_eye_outlined
-                        : showMePass = Icons.remove_red_eye;
-                  });
-                },
-                onChangeText: (onChangePass) =>
-                    setState(() => password = onChangePass)),
-            SizedBox(height: 2.0.h),
-            TextFields(
-                lblText: passwordPlaceHolderNew,
-                maxLen: 20,
-                keyboardType: TextInputType.visiblePassword,
-                readOnly: false,
-                textInputType: protectedPassword,
-                textFieldIcon:
-                    repassword == "" ? Icons.vpn_key_outlined : showMePass,
-                iconPressed: () {
-                  setState(() {
-                    protectedPassword
-                        ? protectedPassword = false
-                        : protectedPassword = true;
-                    // Changing eye icon pressing
-                    showMePass == Icons.remove_red_eye
-                        ? showMePass = Icons.remove_red_eye_outlined
-                        : showMePass = Icons.remove_red_eye;
-                  });
-                },
-                onChangeText: (onChangeRepass) =>
-                    setState(() => repassword = onChangeRepass))
-          ],
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(height: 2.0.h),
+              TextFields(
+                  lblText: passwordPlaceHolder,
+                  maxLen: 20,
+                  keyboardType: TextInputType.visiblePassword,
+                  readOnly: false,
+                  textInputType: protectedPassword,
+                  textFieldIcon:
+                      password == "" ? Icons.vpn_key_outlined : showMePass,
+                  iconPressed: () {
+                    setState(() {
+                      protectedPassword
+                          ? protectedPassword = false
+                          : protectedPassword = true;
+                      // Changing eye icon pressing
+                      showMePass == Icons.remove_red_eye
+                          ? showMePass = Icons.remove_red_eye_outlined
+                          : showMePass = Icons.remove_red_eye;
+                    });
+                  },
+                  onChangeText: (onChangePass) =>
+                      setState(() => password = onChangePass)),
+              SizedBox(height: 2.0.h),
+              TextFields(
+                  lblText: passwordPlaceHolderNew,
+                  maxLen: 20,
+                  keyboardType: TextInputType.visiblePassword,
+                  readOnly: false,
+                  textInputType: protectedPassword,
+                  textFieldIcon:
+                      repassword == "" ? Icons.vpn_key_outlined : showMePass,
+                  iconPressed: () {
+                    setState(() {
+                      protectedPassword
+                          ? protectedPassword = false
+                          : protectedPassword = true;
+                      // Changing eye icon pressing
+                      showMePass == Icons.remove_red_eye
+                          ? showMePass = Icons.remove_red_eye_outlined
+                          : showMePass = Icons.remove_red_eye;
+                    });
+                  },
+                  onChangeText: (onChangeRepass) =>
+                      setState(() => repassword = onChangeRepass))
+            ],
+          ),
         ),
-      ),
-      bottomNavigationBar: BottomButton(
-        text: "تایید ویرایش گذرواژه",
-        ontapped: () => submitPassword(
-            otpCode: otpCode, password: password, repassword: repassword),
+        bottomNavigationBar: BottomButton(
+          text: "تایید ویرایش گذرواژه",
+          ontapped: () => submitPassword(
+              otpCode: otpCode, password: password, repassword: repassword),
+        ),
       ),
     );
   }
