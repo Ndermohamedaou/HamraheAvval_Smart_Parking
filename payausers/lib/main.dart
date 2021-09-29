75ee73c5533bdd21d9d2683a607bba3161979bd2
Asncodes-80 <asn80.asn@hotmail.com>
2021-09-29 21:10:18 +0330

    Changed: initialRoute to splashScreen

--- payausers/lib/main.dart
+++ payausers/lib/main.dart
@@ -250,7 +250,7 @@ class _MyAppState extends State<MyApp> {
                   },
                   theme:
                       Styles.themeData(themeChangeProvider.darkTheme, context),
-                  initialRoute: '/termsAndLicense',
+                  initialRoute: '/splashScreen',
                   routes: {
                     '/splashScreen': (context) => SplashScreen(),
                     '/checkConnection': (context) => CheckConnection(),
